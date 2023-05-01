749c906be6809c0379dd8302846b18027e67e0a2
Youssef El Idrissi <113300812+0xW3ston@users.noreply.github.com>
2023-05-01 06:20:26 +0100

    Update universe.sql
    Idk About that chief

--- universe.sql
+++ universe.sql
@@ -1,7 +1,7 @@
 --
 -- PostgreSQL database dump
 --
-
+-- Another Test (For Pull lol)
 -- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
 -- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
 
